ccf5a6609d47094300527ee62388c0080c47829c
Chor <97508567+tanch-p@users.noreply.github.com>
2022-03-02 17:34:54 +0800

    added router

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { reactive, ref } from "vue";
+import Card from "./components/jokes/Card.vue";
+
+let language = "cn";
+const header = ref(language !== "cn");
+const text = ref(`${language}`);
+const string =
+  "aaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaaaaaa aaaaaaaa aaaaaaaaaaaaaaaaaaaaa   aaaaaaaaaaaaaa aaaaaaaaaaa bbbbbbbbbbbb cccccccccccccc      dddddddddddd eeeeeeeeee ffffffffffffffffffffffffffffff gggggggg";
+const random = ref(string);
+
+const globalShow = ref(false);
+
+</script>
+
+<template>
+  <div class="bg-black text-white text-center">
+    <p class="text-4xl" v-if="header">Moshui</p>
+    <p v-else>墨水</p>
+    <p class="">{{ text }}</p>
+  </div>
+  <div id="navbar" class="h-10 bg-gray-400">
+    About
+    <button @click="()=>globalShow = !globalShow">Show Answers</button>
+    <div class="float-right mr-5">Login</div>
+  </div>
+  <div class="w-min h-min">{{ random }}</div>
+
+  <Card :globalShow="globalShow" />
+</template>
---
+++ src/components/jokes/Card.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+const props = defineProps({
+  globalShow: Boolean
+})
+
+const localShow = ref(false)
+
+</script>
+
+<template>
+  <div class="flex flex-wrap flex-col lg:w-[200px]">
+    <div>test</div>
+    <h1 v-if="globalShow || localShow">Vue is awesome!</h1>
+    <button v-else @click="localShow=!localShow">>Reveal Answer</button>
+  </div>
+</template>
