a3658f34ad06b3c9ee4e69cd08537904eb24dbcb
Chor <97508567+tanch-p@users.noreply.github.com>
2022-03-07 14:56:49 +0800

    changed folder structure

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 
-
-
-const userController = require("./controllers/userController")
+const userController = require("./controllers/userController");
 const jokeController = require("./controllers/jokeController");
 const upvoteController = require("./controllers/upvoteController");
-const commentController = require("./controllers/commentController")
+const commentController = require("./controllers/commentController");
 const replyController = require("./controllers/replyController");
 
 //* CONFIG
@@ -17,7 +15,6 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI;
-
 
 //* CONNECT MONGODB
 mongoose.connection.on("error", (err) =>
@@ -32,12 +29,11 @@ mongoose.connection.once("open", () => {
   console.log("connected to mongoose at " + MONGODB_URI);
 });
 
-
 //* MIDDLEWARE
 app.use(express.static(path.join(__dirname, "./client/build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors())
+app.use(cors());
 
 // app.use(
 //   session({
@@ -46,18 +42,17 @@ app.use(cors())
 //     saveUninitialized:false
 //   })
 //   )
-  
-  //* ROUTES MIDDLEWARE
-  app.use('/api/users', userController)
-  app.use("/api/jokes", jokeController);
-  app.use('/api/upvotes', upvoteController)
-  // app.use('/api/comments', commentController);
-  // app.use('/api/replies', replyController)
- 
+
+//* ROUTES MIDDLEWARE
+app.use("/api/users", userController);
+app.use("/api/jokes", jokeController);
+app.use("/api/upvotes", upvoteController);
+// app.use('/api/comments', commentController);
+// app.use('/api/replies', replyController)
+
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/dist", "index.html"));
 });
-
 
 app.listen(port, () => {
   console.log(`Server is now listening at http://localhost:${port}`);
